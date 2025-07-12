db944147c7fe68e0efa4de8c2478fb817234f5ba
hafeezrafeeq <hafeezrafeeq031@gmai.com>
2025-07-12 23:22:00 +0500

    done

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,53 +3,77 @@
 function works() {
     return (
         <>
-            <div className="flex flex-col items-center pt-20 min-h-screen text-center bg-stone-50 ">
+            <div className="flex flex-col items-center justify-center  pt-20 min-h-screen text-center bg-stone-50  ">
                 <h1 className="text-4xl font-bold mb-6 raleway">How it works?</h1>
                 <p className="text-md text-gray-900 text-center ">TWorking with our team means 100% customer satisfaction. <br />
                     Our english speaking team pays attention to every tiny detail until the final drawing speaks for itself -</p>
                 <h2 className="text-2xl font-bold mt-3 raleway" >Unlimited Revision Rounds </h2>
 
-                <img className="w-180 h-160 absolute " src="https://toonimized.netlify.app/static/media/neclace.116d1ed1.png" alt="" />
+                <img className="w-200 h-160 relative bottom-60 " src="https://toonimized.netlify.app/static/media/neclace.116d1ed1.png" alt="" />
 
 
-                <div className="flex justify-center items-center absolute top-800">
+                <div className="flex justify-center items-center top-780">
                     <div>
                         <div className="custpmizeCircle rounded-full w-30 h-40 ">
                             <img className="w-20 " src="https://toonimized.netlify.app/static/media/family.a94526ff.png" alt="" />
                             <h1 className=" text-6xl raleway font-bold"> 1</h1>
                         </div>
+                        <div className="relative right-10 ">
+                            <p className=" text-2xl font-bold raleway">Single, or...?</p>
+                            <p className=" raleway w-40 text-sm  font-bold">Choose Number of People and pets to be drawn</p>
+                        </div>
                     </div>
+
+
                     <div className="flex flex-col items-center justify-center">
                         <div className="custpmizeCircle rounded-full w-30 h-40 mr-20 mt-100">
                             <img className="w-20 absolute " src="https://toonimized.netlify.app/static/media/family.a94526ff.png" alt="" />
                             <h1 className="pt-18 text-6xl raleway font-bold"> 2</h1>
                         </div>
-                        <p className=" text-2xl font-bold text-start raleway">Single, or...?</p>
-                        <p className=" raleway w-40 text-sm text-start font-bold">Choose Number of People and pets to be drawn</p>
+                        <div className="relative right-10 ">
+                            <p className=" text-2xl font-bold raleway">Single, or...?</p>
+                            <p className=" raleway w-40 text-sm  font-bold">Choose Number of People and pets to be drawn</p>
+                        </div>
                     </div>
+
+
                     <div>
-                        <div className="custpmizeCircle rounded-full w-30 h-40 mt-180 ml-20">
+                        <div className="custpmizeCircle rounded-full w-30 h-40 mt-180 ">
                             <img className="w-20 " src="https://toonimized.netlify.app/static/media/family.a94526ff.png" alt="" />
                             <h1 className=" text-6xl raleway font-bold"> 3</h1>
                         </div>
+                        <div className="relative right-4 ">
+                            <p className=" text-2xl font-bold raleway">Single, or...?</p>
+                            <p className=" raleway w-40 text-sm  font-bold">Choose Number of People and pets to be drawn</p>
+                        </div>
                     </div>
+
+
                     <div>
-                        <div className="custpmizeCircle rounded-full w-30 h-40 mt-100 ml-40 ">
+                        <div className="custpmizeCircle rounded-full w-30 h-40 mt-100 ml-20 ">
                             <img className="w-20 " src="https://toonimized.netlify.app/static/media/family.a94526ff.png" alt="" />
                             <h1 className=" text-6xl raleway font-bold"> 4</h1>
                         </div>
+                        <div className="relative left-20 ">
+                            <p className=" text-2xl font-bold raleway">Single, or...?</p>
+                            <p className=" raleway w-40 text-sm  font-bold">Choose Number of People and pets to be drawn</p>
+                        </div>
                     </div>
+
                     <div>
-                        <div className="custpmizeCircle rounded-full w-30 h-40 ">
+                        <div className="custpmizeCircle rounded-full w-30 h-40 ml-10">
                             <img className="w-20 " src="https://toonimized.netlify.app/static/media/family.a94526ff.png" alt="" />
                             <h1 className=" text-6xl raleway font-bold"> 5</h1>
+                        </div>
+                        <div className="relative left-4 ">
+                            <p className=" text-2xl font-bold raleway">Single, or...?</p>
+                            <p className=" raleway w-40 text-sm  font-bold">Choose Number of People and pets to be drawn</p>
                         </div>
                     </div>
                 </div>
 
 
-                <div className="custpmizeCircle w-60 h-60 rounded-full mt-12">
-                </div>
+                <div className="custpmizeCircle w-60 h-60 rounded-full mt-12 relative bottom-160 "></div>
 
             </div>
         </>
