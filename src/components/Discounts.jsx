0031bc8cb99fb3f3fff4e2ed472922d2e8da0e35
hafeezrafeeq <hafeezrafeeq031@gmai.com>
2025-07-13 20:56:13 +0500

    discount contanier is complete

--- src/components/Discounts.jsx
+++ src/components/Discounts.jsx
@@ -2,7 +2,7 @@ function discounts() {
     <div>
         <div className="flex flex-col  w-400">
 
-            <h1 className="">Who doesn’t love discounts? – Get 15% off</h1>
+            <h1 className="">Who doesn’t love Discounts? – Get 15% off</h1>
 
             <p>Enter your email and get 15% discount on your total order,
                 also you’ll be in touch with our latest discounts and news.
